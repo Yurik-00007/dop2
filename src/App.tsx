bdd2102b6d9676de314d8f96888a37fa3d823052
Timofeev Yury <yurik-007@mail.ru>
2022-10-28 19:32:53 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import {Button} from "./components/Button";
+
+type ShowType={
+    userId: number
+    id: number
+    title: string
+    completed: boolean
+}
 
 function App() {
+
+  const [show,setShow]=useState<ShowType[]>([])
+
+    //автоматом при загрузки страницы
+  /*useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => setShow(json))
+  }, [])*/
+
+  const showUP=()=>{
+      fetch('https://jsonplaceholder.typicode.com/todos')
+          .then(response => response.json())
+          .then(json => setShow(json))
+  }
+
+  const clean=()=>setShow([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Button name={'ShowMe'} callBack={showUP}/>
+<Button name={'CleanMe'} callBack={clean}/>
+      <ul>
+
+        {show.map(el=>{
+        return(
+        <li key={el.id}>
+            <span>{el.userId}</span>
+            <span>{el.id}</span>
+            <span>{el.title}</span>
+            <input style={{marginRight:'50px'}} type={'checkbox'} checked={el.completed}/>
+
+        </li>
+        )
+      })}
+      </ul>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+
+type ButtonType={
+    name:string
+    callBack:()=>void
+}
+
+
+export  const Button=(props:ButtonType)=>{
+    const{name,callBack}=props
+    const onClickHandler=()=>{
+        callBack();
+    }
+    return(
+        <button onClick={onClickHandler}>{name}</button>
+    )
+}
